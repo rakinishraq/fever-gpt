30ecf1194d0a66510d8b705c855c198010b91cf8
name <email>
2023-11-09 03:37:14 -0500

    /setting added, elif soup redo, google search

--- README.md
+++ README.md
@@ -1,20 +1,70 @@
-config.py
+# FeverGPT
+
+FeverGPT is released under the [0BSD License](COPYING).
+- The author is not responsible for any damage, loss, or issues arising from the use or misuse of this 0BSD licensed software.  
+The summary below is from [tl;drLegal](https://www.tldrlegal.com/license/bsd-0-clause-license).
+- The BSD 0-clause license goes further than the 2-clause license to allow you unlimited freedom with the software without the requirements to include the copyright notice, license text, or disclaimer in either source or binary forms.
+
+## Plugins
+
+This tool uses a lightly modified version of [andylokandy's GPT4 CLI tool](https://github.com/andylokandy/gpt-4-search) as a temporary backend, which is released under an [MIT license](https://github.com/rakinishraq/gpt-4-search/blob/main/LICENSE).
+- [EvAnhaodong's fork](https://github.com/EvAnhaodong/gpt-4-search) was considered but was too bloated for a, likely hosted in a low-performance container, Discord bot. Like their fork, however, end-to-end testing will be implemented before adding additional functionality.
+- From my testing, this backend was meant to only work on Linux, since the readline module is Linux-only and commonly answering "I don't know" without it mostly for Python responses. As a temporary solution, I removed the import line but a plugin-free backend is also provided (leave BACKEND_PATH as "" to only use that).
+- This backend also seems to have dubious support for models other than GPT4. While testing with GPT3.5-turbo, it outputs a parsing error when trying to summarize a Google result and output it. Thus, this bot only works with GPT4 for now. The scaffolding is in place to support other models as soon as possible.
+- **To download and use my fork,**  go to the [repository](https://github.com/rakinishraq/gpt-4-search) and follow the instructions to install. Then, enter the path your `gpt_4_search.py` file in `config.py`.
+
+The system prompt for the plugins system:
+> You are an _[sic]_ helpful and kind assistant to answer questions that can use tools to interact with real world and get access to the latest information. You can call one of the following functions:
+In each response, you must start with a function call like `SEARCH(\"something\")` or `PYTHON(\"\"\"1+1\"\"\")`. Don't explain why you use a tool. If you cannot figure out the answer, you say ’I don’t know’. When you are generating answers according to the search result, link your answers to the snippet id like `[1]`, and use the same language as the questioner.
+
+Function prompts:
+- **SEARCH:** searches the web, and returns the top snippets, it'll be better if the query string is in english
+- **SUMMARIZE:** click into the search result, useful when you want to investigate the detail of the search result
+- **PYTHON:** evaluates the code in a python interpreter, wrap code in triple quotes, wrap the answer in `print()`
+
+The user-inputted system prompt would be added before everything else.
+
+## Todo
+
+Another feature that will be implemented is using files (plaintext and PDFs/DOCX/etc.) as context, leveraging Discord's file upload feature and support for direct links and a common filehosting service like Google Drive. The free user filesize limit shouldn't be an issue in this context, however.
+Lastly, a streaming solution will be implemented.
+
+- code execution security
+- test working channel prompt
+- add grants
+- test boolean stack
+- sanity check on model name
+
+## Usage
+
+If you are using any GPT4 model, the plugins will be enabled by default if `--fallback` is not present. Similarly, GPT3 models will default to fallback if `--plugins` is not present.
+
+- `/setting` resets the channel's settings
+- `/setting <model/prompt>` prints the channel's model/prompt
+- `/setting model <model_name>` changes the model to gpt-4, gpt-3.5turbo, etc.
+- `/setting prompt <system_prompt>` changes the system prompt
+
+
+## Configuration
+
+Create a `config.py` file in the same directory as bot.py and open it with any text editor, like Notepad. Then, enable Developer Mode in Discord and enter the following information:
 
 ```
+# if you want to lock bot to channel categories, enter category IDs
+CATEGORY = []
+# [YourBot > Bot > Token] in discord.com/developers/applications
 TOKEN = "DISCORD BOT TOKEN"
-GUILD = "SERVER NAME"
+# if you want to lock bot to server and their members, enter server IDs
+GUILD = []
+# if you want to lock to certain users, enter user IDs
+USER_ID = []
 
+BACKEND_PATH = "C:/Path/To/gpt_4_search.py"
 API_KEY = "OPENAI API KEY"
-DEFAULT = "You are a helpful assistant. Answer concisely unless if specified otherwise."
-MODEL = "gpt-4"
+DEFAULT = ["You are a Discord bot for GPT named FeverGPT.", "gpt-4"]
 
 CHANNELS = "path/to/channels.json"
 ERRORS = "path/to/errors.log"
 
 NO_GPT = False
 ```
-
-FeverGPT is released under the [0BSD License]().
-- The author is not responsible for any damage, loss, or issues arising from the use or misuse of this 0BSD licensed software.  
-The summary below is from [tl;drLegal](https://www.tldrlegal.com/license/bsd-0-clause-license).
-- The BSD 0-clause license goes further than the 2-clause license to allow you unlimited freedom with the software without the requirements to include the copyright notice, license text, or disclaimer in either source or binary forms.
--- bot.py
+++ bot.py
@@ -6,15 +6,20 @@ from config import *
 from sys import exc_info
 import openai
 import json
+import importlib.util
+
+if BACKEND_PATH:
+    spec = importlib.util.spec_from_file_location("module.name", BACKEND_PATH)
+    backend = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(backend)
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix="/", intents=intents)
 
-
-global channel_prompts
+global channel_data
 with open(CHANNELS, 'a+') as f:
-    channel_prompts = json.load(f)
+    channel_data = json.load(f) if f.read() else {}
 
 
 @client.command(name="shutdown", brief="Shut ChatGPT down", help="Shut ChatGPT down.",
@@ -23,17 +28,27 @@ async def shutdown(ctx):
     await ctx.send(f"I'm afraid I can't do that, {ctx.author.mention}.")
 
 
-@client.command(name="prompt", brief="Change the system prompt for this channel",
-        help="Usage: /prompt [new_prompt]. Replace [new_prompt] with your desired system prompt.")
-async def change_prompt(ctx, *, new_prompt=None):
-    if not new_prompt:
-        current_prompt = channel_prompts.get(ctx.channel.id, DEFAULT)
-        await ctx.send(f"Current prompt: {current_prompt}")
+@client.command(name="setting", brief="Change the system prompt and/or model for this channel",
+        help="Usage: /setting prompt [new_prompt] or /setting model [new_model] or /setting reset.")
+async def setting(ctx, setting, *, new_value=None):
+    if setting == "reset":
+        await ctx.send("Defaults applied to this channel.\nModel: %s\nPrompt: %s"%DEFAULT)
+        return
+    elif setting not in ("prompt", "model"):
+        await ctx.send("Invalid setting. Use prompt/model/reset.")
+        return
+
+    index = 0 if setting == "prompt" else 1
+    if not new_value:
+        current_value = channel_data.get(ctx.channel.id, DEFAULT)[index]
+        await ctx.send(f"Current channel {setting}: {current_value}")
     else:
-        channel_prompts[ctx.channel.id] = new_prompt
+        if ctx.channel.id not in channel_data:
+            channel_data[ctx.channel.id] = DEFAULT
+        channel_data[ctx.channel.id][index] = new_value
         with open(CHANNELS, "w") as f:
-            json.dump(channel_prompts, f)
-        await ctx.send(f"System prompt for this channel changed to: {new_prompt}")
+            json.dump(channel_data, f)
+        await ctx.send(f"{setting.capitalize()} for this channel changed to: {new_value}")
 
 
 @client.event
@@ -49,42 +64,69 @@ async def on_ready():
     print(f'{client.user.name} is online.')
     await client.change_presence(activity=discord.Game(name="with human lives"))
 
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    if not guild:
-        return
-
 
 @client.event
 async def on_message(message):
+    send = message.channel.send
+
+    # process slash commands
     if message.content.startswith("/"):
         await client.process_commands(message)
         return
+    # prevent self-reply loop
     elif message.author == client.user:
         return
-    elif isinstance(message.channel, discord.channel.DMChannel):
-        pass
-    elif message.channel.category_id != 1171050881626681364:
-        return
-    
-    if message.author.id != 748021473070940163:
-        await message.channel.send("GPT grants not implemented for non-owner users yet.")
+    elif GUILD:
+        # check if DM user is in any approved guild
+        if isinstance(message.channel, discord.channel.DMChannel):
+            for guild_id in GUILD:
+                if client.get_guild(guild_id).get_member(message.author.id):
+                    break
+            else:
+                return
+    elif CATEGORY:
+        # lock to threads in approved category
+        if isinstance(message.channel, discord.channel.Thread):
+            if not message.channel.parent.category_id in CATEGORY:
+                return
+        # lock to text channels if approved category
+        elif not message.channel.category_id in CATEGORY:
+            return
+    # lock to approved user
+    if USER_ID and not message.author.id in USER_ID:
+        await send("GPT grants not implemented for non-owner users yet. "+message.author.id)
         return
 
-    system_prompt = channel_prompts.get(message.channel.id, DEFAULT)
+    system_prompt, model = channel_data.get(message.channel.id, DEFAULT)
 
     if NO_GPT:
-        await message.channel.send("Test mode enabled (no GPT API calls).")
+        await send("Test mode enabled (no GPT API calls). Model: "+model)
         return
 
-    response = openai.ChatCompletion.create(
-      model=MODEL,
-      messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": message.content},
-        ]
-    )
+    async with message.channel.typing():
+        msg = message.content.replace('--plugins', '').replace('--fallback', '')
+        # plugins mode default for gpt4, --plugins override for gpt3
+        if BACKEND_PATH and "--fallback" not in message.content:
+            backend.MARKDOWN_MODS = channel_data.get(message.channel.id, DEFAULT)
+            if "gpt-4" in model or "--plugins" in message.content:
+                try:
+                    await send(backend.run(msg))
+                    if backend.references:
+                        await send("\n\n**References:**")
+                        await send(backend.show_references())
+                    return
+                except Exception as e:
+                    await send(f"`Error: {str(e)}`\nRetrying without plugin:")
 
-    await message.channel.send(response['choices'][0]['message']['content'])
+        # fallback
+        response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": msg},
+            ]
+        )
+        await send(response['choices'][0]['message']['content'])
 
 
 if __name__ == "__main__":
